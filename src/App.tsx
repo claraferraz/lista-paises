93a59b7be854ca8c1008064542de84dee7bc2558
Clara Ferraz <claraferraz@proton.me>
2025-08-09 12:20:12 -0300

    Styles: change heart color on header when switch pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Header } from "./components/Header/Header";
 import { Wrapper } from "./components/Wrapper/Wrapper";
 import { MainRoutes } from "./routes/MainRoutes";
 
 function App() {
   return (
     <Wrapper>
-      <Header />
       <MainRoutes />
     </Wrapper>
   );
--- src/pages/FavoritesPages.tsx
+++ src/pages/FavoritesPages.tsx
@@ -1,3 +1,9 @@
+import { Header } from "../components/Header/Header";
+
 export const FavoritesPage = () => {
-  return <></>;
+  return (
+    <>
+      <Header fav={true} />
+    </>
+  );
 };
