74f8ab96632a5ceef905e2b31655e13e85e98c10
Clara Ferraz <claraferraz@proton.me>
2025-08-09 15:39:58 -0300

    Feat: home paege with api list

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { CardSection } from "../components/CardSection/CardSection";
 import { CountryCard } from "../components/CountryCard/CountryCard";
-import { brasil } from "../interface/countryDTO";
+import { useCountry } from "../context/CountryContext";
+
 import styles from "./styles.module.css";
 
 export const HomePage = () => {
+  const { list } = useCountry();
   return (
     <section>
       <div className={styles.header}>
@@ -13,10 +15,9 @@ export const HomePage = () => {
         <p>filter</p>
       </div>
       <CardSection>
-        <CountryCard country={brasil} liked={false} />
-        <CountryCard country={brasil} liked={true} />
-        <CountryCard country={brasil} liked={true} />
-        <CountryCard country={brasil} liked={true} />
+        {list?.map((l, i) => {
+          return <CountryCard key={i} country={l} liked={false} />;
+        })}
       </CardSection>
     </section>
   );
